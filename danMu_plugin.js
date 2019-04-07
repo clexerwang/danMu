421376f5001eaf65ebea6e26011b56b12259ae55
clexerwang <diligentwang@bupt.edu.cn>
2019-04-07 20:32:42 +0800

    v0.1

--- danMu_plugin.js
+++ danMu_plugin.js
@@ -1,118 +1,60 @@
-(function(){
-   document.addEventListener('click',function(e){
-
-        if(e.target === play){
-            video.paused?video.play():video.pause()
-            clearTimeout(timer) 
-            timer = setTimeout(flagGrow,1000);
-        }
-        if(e.target === belt || e.target === flag){
-            video.currentTime = ((e.clientX-belt.getBoundingClientRect().left)/belt.offsetWidth)*video.duration;
-            flag.style.width = (video.currentTime / video.duration * belt.offsetWidth)+'px';
-            video.play();
-        }
-        if(e.target === send){
-            danmuP.setDanMU(form.elements['color'].value,Boolean(parseInt(form.elements['position'].value)),form.elements['input'].value,video.currentTime);
-            form.elements['input'].value = '';
-        }
-        
-    })
-
-    document.addEventListener('change',function(e){
-        if(e.target === show){
-            if(!Boolean(parseInt(e.target.value))){
-                canvas3.style.display = 'none';
-            }else{
-                canvas3.style.display = 'block'
-            }
-        }
-    })
-
-    document.addEventListener('mouseenter',function(e){
-        if(e.target === belt){
-            e.target.background.color = 'gray'
-        }
-    })
-    function flagGrow(){
-        timer = requestAnimationFrame(flagGrow,100);
-        flag.style.width = (video.currentTime / video.duration * belt.offsetWidth)+'px';
-    }
-    loadingDanMu();
-    function loadingDanMu(){
-        setTimeout(function(){
-            if(arr.length){
-                let fragment = document.createDocumentFragment();
-                for(let i =0;i<arr.length;i++){
-                    let danmuL = document.createElement('div');
-                    let time = document.createElement('div');
-                    let text = document.createElement('div');
-                    danmuL.style.cssText = 'display:flex;padding:5px 0 0 5px;font-size:14px;line-height:20px;'
-                    time.innerText = `${Math.floor(arr[i].time/60)}:${(arr[i].time % 60).toFixed(0).length === 1?'0'+(arr[i].time % 60).toFixed(0):(arr[i].time % 60).toFixed(0)} `
-                    text.innerText = `${arr[i].text}`
-                    
-                    time.style.cssText = 'width:15%;'
-                    text.style.cssText = 'width:85%;overflow:hidden; white-space:nowrap;text-overflow:ellipsis;'
-                    
-                    danmuL.appendChild(time);
-                    danmuL.appendChild(text);
-                    fragment.appendChild(danmuL)
-                }
-                danMuList.appendChild(fragment)
-            }else{
-              loadingDanMu();
-            }
-        },0)
-    }
-
-    
 
 
 
 
-    function DanMuPlayer(video,canvas){
+
+    function DanMuPlayer(video,canvas,canvasWidth,canvasHeight,trackHight){
         this.danMu = null;
         this.suspend = true;
         this.context = null;
         this.video = video;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        //提供弹幕信息的canvas
         this.canvas = document.createElement('canvas');
         this.canvas.height=canvasHeight;
         this.canvas.width=canvasWidth;
-        this.control = new Control(50)
 
+        //最终渲染的canvas
+        this.renderCanvas = canvas;
+        this.renderCanvasContext = canvas.getContext('2d');
+        
+        //提供视频信息的canvas
+        this.videoCanvas = document.createElement('canvas');
+        videoCanvas.width=canvasWidth;
+        videoCanvas.height=canvasHeight;
+        this.videoCanvasContext = videoCanvas.getContext('2d');
+
+        this.control = new Control(trackHight)
     }
 
     DanMuPlayer.prototype={
         constructor:DanMuPlayer,
         setDanMU:function(color,fixed,text,currentTime){
-
             this.danMu.push({
-                text:text,color:color,fontSize:50,x:canvasWidth,fixed:fixed,width:this.context.measureText(text).width*6,time:currentTime,hasPlayed:false,ifInTrack:false,hasInTrack:false,isRendering:false
+                text:text,
+                color:color,
+                fontSize:50,
+                x:this.canvasWidth,
+                fixed:fixed,
+                width:this.context.measureText(text).width*4+100,
+                time:currentTime,
+                hasPlayed:false,
+                ifInTrack:false,
+                hasInTrack:false,
+                isRendering:false
             })
 
         },
         init:function(arr){
             let self = this;
-            
             self.control.init();
             self.danMu = arr;
             self.context = self.canvas.getContext('2d');
             self.context.strokeStyle = 'black';
-            
-                            
-            
-            //绑定事件
-            self.video.addEventListener('play',(e)=>{
-                self.suspend = false;
-                self.render();
-            })
-            self.video.addEventListener('pause',(e)=>{
-                self.suspend = true;
-            })
-
-            self.video.addEventListener('seeking',(e)=>{
  
+            self.video.addEventListener('seeking',(e)=>{
                 this.context.clearRect(0,0,self.canvas.width,self.canvas.height);
-
                 for(let i = 0;i<self.danMu.length;i++){
                     if(self.danMu[i].time < self.video.currentTime){
                         //当前时间以前的弹幕设为已经渲染过，将当前时间以后的弹幕初始化
@@ -125,7 +67,7 @@
                         self.danMu[i].hasInTrack = false;
                         self.danMu[i].ifInTrack = false;
                         self.danMu[i].isRendering = false;
-                        self.danMu[i].x = canvasWidth;
+                        self.danMu[i].x = self.canvasWidth;
                     }
                 }
                     
@@ -153,7 +95,7 @@
                         }else{
                             this.danMu[i].hasPlayed = false;
                             this.danMu[i].hasInTrack = false;
-                            this.danMu[i].x = canvasWidth;
+                            this.danMu[i].x = self.canvasWidth;
                         }
                     }
                 }
@@ -166,25 +108,25 @@
             //弹幕防档画面
             
                 //用来渲染视频，图像对象，原始图像中的起始x坐标,原始图像中的起始y坐标,宽度，高度
-                context1.drawImage(video,0,0,canvasWidth,canvasHeight);
+                self.videoCanvasContext.drawImage(video,0,0,self.canvasWidth,self.canvasHeight);
                 //获取图像信息，起始x坐标，起始y左边，宽度，高度
-                let canvas1Data = context1.getImageData(0,0,canvasWidth,canvasHeight);
+                let videoCanvasData = self.videoCanvasContext.getImageData(0,0,self.canvasWidth,self.canvasHeight);
 
                 //获取弹幕信息
-                let trueData = self.context.getImageData(0,0,canvasWidth,canvasHeight);
+                let renderData = self.context.getImageData(0,0,self.canvasWidth,self.canvasHeight);
                 //修改
-                for(let i=0;i<canvas1Data.data.length/4;i++){
-                    const r = canvas1Data.data[i * 4 + 0];
-                    const g = canvas1Data.data[i * 4 + 1];
-                    const b = canvas1Data.data[i * 4 + 2];
+                for(let i=0;i<videoCanvasData.data.length/4;i++){
+                    const r = videoCanvasData.data[i * 4 + 0];
+                    const g = videoCanvasData.data[i * 4 + 1];
+                    const b = videoCanvasData.data[i * 4 + 2];
 
                     if (r > 15 || g > 15 || b > 15) {
-                      trueData.data[4 * i + 3] = 0;
+                      renderData.data[4 * i + 3] = 0;
                     }
                 }
 
                 //最终渲染，图像对象，渲染位置的起始x坐标，渲染位置的起始y坐标，源数据的起始x位置，源数据的起始y位置，渲染的宽度，渲染的高度
-                context3.putImageData(trueData,0,0,0,0,canvasWidth,canvasHeight);
+                self.renderCanvasContext.putImageData(trueData,0,0,0,0,this.canvasWidth,this.canvasHeight);
 
             
             
@@ -235,10 +177,27 @@
                     }
                 }
             }
+        },
+        //暂停弹幕
+        suspend:function(){
+            this.suspend = true;
+        },
+        //开始播放弹幕
+        play:function(){
+            this.suspend = false;
+            this.render();
+        },
+        //弹幕隐藏
+        hidden:function(){
+            this.renderCanvas.style.display = 'none';
+        },
+        show:function(){
+            this.renderCanvas.style.display = 'block';
         }
+
     } 
 
-    function seekThrottle(seek,delay){
+/*    function seekThrottle(seek,delay){
         let timer = null;
         let startDate = Date.now();
 
@@ -254,7 +213,7 @@
             }
 
         }
-    }
+    }*/
 
     //轨道类，负责调度弹幕的渲染位置
     function Track(){
@@ -301,15 +260,17 @@
     }
 
     //负责轨道类的调度
-    function Control(){
+    function Control(trackHight,trackNum){
         this.trackes = [];
+        this.trackHight = trackHight;
+        this.trackNum = trackNum;
     }
 
     Control.prototype = {
         constructor:Control,
         init:function(){
             //根据视频高度生成轨道
-            for(let i =0;i< 20;i++){
+            for(let i =0;i< this.trackNum||20;i++){
                 this.trackes.push(new Track())
             }
         },
@@ -317,7 +278,7 @@
         receiveWork:function(danMu){
             //判断弹幕类型，是固定还是滚动
             if(danMu.fixed){
-                for(let i=0;i<20;i++){
+                for(let i=0;i< this.trackNum||20;i++){
                     if(!this.trackes[i].fixed){
                         let finalDanMu = calculY(danMu,i);
                         finalDanMu.isRendering = true;
@@ -326,7 +287,7 @@
                 }  
             }else{
                 //遍历track，寻找空闲track
-                for(let i=0;i<20;i++){
+                for(let i=0;i< this.trackNum||20;i++){
                     if(!this.trackes[i].busy){
                         //开始对弹幕进行初始化(计算弹幕纵向初始位置),然后准备渲染
                         let finalDanMu = calculY(danMu,i);
@@ -335,8 +296,7 @@
                         return this.trackes[i].push(finalDanMu);
                     }
                 }  
-            }
-                    
+            }               
         },
         
     }
@@ -348,75 +308,9 @@
         }
         return {...danMu,y:(index+1)*50};
     }
- 
-
-    let canvasWidth,canvasHeight;
-    let belt = document.querySelector('#belt');
-    let play = document.querySelector('#play');
-    let input = document.querySelector('#input');
-    let send = document.querySelector('#send');
-    let show = document.querySelector('#show');
-    let flag = document.querySelector('#flag');
-    let relateX;
-    let timer;
-    let form = document.querySelector('form');
-    let danMuList = document.querySelector('#danMuList')
-    let trackHeight = 50;
-    let arr=[];
-    let danmuP;
     
 
-    
-    let video = document.querySelector('video');
-    //danMuCreator(arr,500);
-    
-    
+    danmuP = new DanMuPlayer(video);
+    danmuP.init(arr);
 
 
-    //获取视频信息canvas
-    let canvas1 = document.createElement('canvas');
-    canvas1.width=canvasWidth;
-    canvas1.height=canvasHeight;
-    let context1 = canvas1.getContext('2d');
-
-    //渲染canvas
-    let canvas3 = document.querySelector('#canvas3');
-    let context3 = canvas3.getContext('2d');
-    
-    fetch('http://localhost:3000/danmu').then(function(res){
-        res.json().then(function(result){
-            arr = getDanmu(result);
-            console.log(arr)
-            //弹幕渲染
-            danmuP = new DanMuPlayer(video);
-            danmuP.init(arr);
-
-        })
-    })
-
-
-
-function getDanmu(info){
-  let result = [];
-  let canvas = document.createElement('canvas');
-  let context = canvas.getContext('2d')
-  return info.map((eve)=>{
-      let num = Math.floor(Math.random()*100);
-      return {
-        text:eve.text,
-        color:num <40 ? num>20?'yellow':'red':'white',
-        fontSize:50,
-        x:canvasWidth,
-        fixed:Math.floor(Math.random()*800)<80?true:false,
-        width:context.measureText(eve.text).width*4+100,
-        time:eve.time,
-        hasPlayed:false,
-        ifInTrack:false,
-        hasInTrack:false,
-        isRendering:false
-      }
-  }) 
-}
-
-
-})()
